b226d64cae5d28cfbafa3afc196f525c592df77f
serghei madan <madan.serghei@yahoo.com>
2023-10-12 11:09:43 -0700

    BACKEND : added get all users route and controller

--- backend/controllers/Product/GetAllProducts.js
+++ backend/controllers/Product/GetAllProducts.js
@@ -4,6 +4,10 @@ export const getAllProducts = async (req, res) => {
   try {
     const allProducts = await Product.find({});
 
+    if(!allProducts){
+      res.status(400).json({message: "no products found", allProducts: []})
+    }
+
     res.status(200).json(allProducts);
     
   } catch (error) {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,15 @@
 import express from "express";
-import productsRoutes from "./Products/products.js";
 import authRoutes from "./Auth/auth.js";
+import productsRoutes from "./Products/products.js";
+import usersRoutes from './Users/users.js'
 import presentationRoutes from "./Presentation/presentation.js";
 import isLoggedIn from "../middleware/auth/isLoggedIn.js";
 
 const router = express.Router();
 
-router.use("/products", isLoggedIn, productsRoutes);
 router.use("/auth", authRoutes);
+router.use("/products", isLoggedIn, productsRoutes);
+router.use("/users", usersRoutes);
 router.use("/presentation", presentationRoutes);
 
 export default router;
