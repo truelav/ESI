1a0b3bd4c0ba25be76b8cc9e92a381e5db3ea0be
serghei madan <madan.serghei@yahoo.com>
2023-10-11 17:28:01 -0700

    FRONTEND: auth using RTK Query

--- frontend/src/app/api/apiSlice.ts
+++ frontend/src/app/api/apiSlice.ts
@@ -1,7 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const fBaseQuery = fetchBaseQuery({ baseUrl: "http://localhost:8888" })
+
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8888" }),
+  baseQuery: fBaseQuery,
   tagTypes: ["Product", "User"],
+  
   endpoints: (builder) => ({}),
 });
