ec24872d5fbc1016cdf7cd8bdb0998e2e3870c27
serghei madan <madan.serghei@yahoo.com>
2023-10-23 17:28:54 -0700

    BACKEND : added add multiple products controller and route

--- backend/controllers/Product/AddMultipleProducts.js
+++ backend/controllers/Product/AddMultipleProducts.js
@@ -9,37 +9,37 @@ export const addMultipleProducts = async (req, res) => {
       const newProducts = []
         // Parse the document into the fields and add each Product
       streamifier.createReadStream(buffer)
-        .pipe(parse({delimiter: ","}))
+        .pipe(parse({delimiter: ",", ignoreEmpty: true}))
         .on('data', (row) => {
-          // const newProduct = new Product({
-          //   name: row.name,
-          //   brand: row.brand,
-          //   description: row.description,
-          //   category: row.category,
-          //   quantity: row.quantity,
-          //   images: row.images,
-          //   location: row.location,
-          //   subcategory: row.upc
-          // });
+          const newProduct = new Product({
+            name: row.Brand,
+            brand: row.Brand,
+            description: row.Description,
+            category: row.Category,
+            quantity: row["Qty's"],
+            images: row.images,
+            location: "Los Angeles",
+            subcategory: row.UPC
+          });
 
-          newProducts.push(row)
+          newProducts.push(newProduct)
         })
-        .on('end', () => {
+        .on('end', async () => {
           console.log(newProducts);
           // [
           //   { NAME: 'Daffy Duck', AGE: '24' },
           //   { NAME: 'Bugs Bunny', AGE: '22' }
           // ]
-          // Product.insertMany(newProducts)
-          //   .then(() => {
-          //     res.status(200).json(`success, all ${newProducts.length} were added`);
-          //   })
-          //   .catch((error) => {
-          //     console.error(error);
-          //     res.status(500).send('Internal Server Error');
-          //   })
+          await Product.insertMany(newProducts)
+            .then(() => {
+              res.status(200).json(`success, all ${newProducts.length} were added`);
+            })
+            .catch((error) => {
+              console.error(error);
+              res.status(500).send('Internal Server Error');
+            })
         });
-        res.status(200).json(`success, all ${newProducts.length} were added`);
+        // res.status(200).json(`success, all ${newProducts.length} were added`);
       } catch (error) {
         console.log(error)
         res.status(500).json({ message: error });
