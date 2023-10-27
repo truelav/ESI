156a6c42baec1bc72e228b5a1f693ca82470aa48
serghei madan <madan.serghei@yahoo.com>
2023-10-27 10:04:40 -0700

    FRONTEND: refactor and redesign of Header Nav and Sidebar

--- frontend/src/components/sidebar/index.tsx
+++ frontend/src/components/sidebar/index.tsx
@@ -1,31 +1,182 @@
-import { NavLink } from "react-router-dom";
-import { Card } from "@chakra-ui/react";
+'use client'
 
-import "./styles.css";
+import {
+  IconButton,
+  Box,
+  CloseButton,
+  Flex,
+  Icon,
+  useColorModeValue,
+  Text,
+  Drawer,
+  DrawerContent,
+  useDisclosure,
+  BoxProps,
+  FlexProps,
+} from '@chakra-ui/react'
+import {
+  FiHome,
+  FiTrendingUp,
+  FiCompass,
+  FiStar,
+  FiSettings,
+  FiMenu,
+} from 'react-icons/fi'
+import { IconType } from 'react-icons'
+import { ReactText } from 'react'
 
-export default function Sidebar() {
-  return (
-    <Card className="sidebar-wrapper" bg="gray.300">
-      <nav className="sidebar-nav">
-        <NavLink to="/dashboard" className="sidebar-navlink">
-          Dashboard
-        </NavLink>
-        <NavLink to="products" className="sidebar-navlink">
-          Products
-        </NavLink>
-        <NavLink to="inventory" className="sidebar-navlink">
-          Inventory
-        </NavLink>
-        <NavLink to="orders" className="sidebar-navlink">
-          Orders
-        </NavLink>
-        <NavLink to="users" className="sidebar-navlink">
-          Users
-        </NavLink>
-        <NavLink to="presentation" className="sidebar-navlink">
-          Presentations
-        </NavLink>
-      </nav>
-    </Card>
-  );
+interface LinkItemProps {
+  name: string
+  icon: IconType
+  link: string
 }
+const LinkItems: Array<LinkItemProps> = [
+  { name: 'dashboard', icon: FiHome, link: "" },
+  { name: 'products', icon: FiCompass, link: "products" },
+  { name: 'presentation', icon: FiStar, link: "presentation" },
+  { name: 'inventory', icon: FiTrendingUp, link: "inventory" },
+  { name: 'users', icon: FiSettings, link: "users" },
+]
+
+export default function SimpleSidebar() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+      <SidebarContent onClose={() => onClose} display={{ base: 'none', md: 'block' }} />
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full">
+        <DrawerContent>
+          <SidebarContent onClose={onClose} />
+        </DrawerContent>
+      </Drawer>
+      {/* mobilenav */}
+      <MobileNav display={{ base: 'flex', md: 'none' }} onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p="4">
+        {/* Content */}
+      </Box>
+    </Box>
+  )
+}
+
+interface SidebarProps extends BoxProps {
+  onClose: () => void
+}
+
+const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
+  return (
+    <Box
+      bg={useColorModeValue('white', 'gray.900')}
+      borderRight="1px"
+      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+      w={{ base: 'full', md: 60 }}
+      pos="fixed"
+      h="full"
+      {...rest}>
+      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+          Admin Name
+        </Text>
+        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+      </Flex>
+      {LinkItems.map((link) => (
+        <NavItem key={link.name} icon={link.icon} link={link.link}>
+          {link.name}
+        </NavItem>
+      ))}
+    </Box>
+  )
+}
+
+interface NavItemProps extends FlexProps {
+  icon: IconType
+  link: string
+  children: ReactText
+}
+const NavItem = ({ icon, link , children, ...rest }: NavItemProps) => {
+  return (
+    <Box
+      as="a"
+      href={`/dashboard/${link}`}
+      style={{ textDecoration: 'none' }}
+      _focus={{ boxShadow: 'none' }}>
+      <Flex
+        align="center"
+        p="4"
+        mx="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: 'cyan.400',
+          color: 'white',
+        }}
+        {...rest}>
+        {icon && (
+          <Icon
+            mr="4"
+            fontSize="16"
+            _groupHover={{
+              color: 'white',
+            }}
+            as={icon}
+          />
+        )}
+        {children}
+      </Flex>
+    </Box>
+  )
+}
+
+interface MobileProps extends FlexProps {
+  onOpen: () => void
+}
+const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+  return (
+    <Flex
+      ml={{ base: 0, md: 60 }}
+      px={{ base: 4, md: 24 }}
+      height="20"
+      alignItems="center"
+      bg={useColorModeValue('white', 'gray.900')}
+      borderBottomWidth="1px"
+      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+      justifyContent="flex-start"
+      {...rest}>
+      <IconButton
+        variant="outline"
+        onClick={onOpen}
+        aria-label="open menu"
+        icon={<FiMenu />}
+      />
+
+      <Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold" textTransform="uppercase">
+        Logo
+      </Text>
+    </Flex>
+  )
+}
+
+
+{/* <NavLink to="/dashboard" className="sidebar-navlink">
+Dashboard
+</NavLink>
+<NavLink to="products" className="sidebar-navlink">
+Products
+</NavLink>
+<NavLink to="inventory" className="sidebar-navlink">
+Inventory
+</NavLink>
+<NavLink to="orders" className="sidebar-navlink">
+Orders
+</NavLink>
+<NavLink to="users" className="sidebar-navlink">
+Users
+</NavLink>
+<NavLink to="presentation" className="sidebar-navlink">
+Presentations
+</NavLink> */}
