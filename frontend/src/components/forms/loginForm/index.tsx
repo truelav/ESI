20ff700662f13c0634f0b47392eea1b769050bc9
unknown <madan.serghei@yahoo.com>
2023-10-30 08:12:40 -0700

    FRONTEND : login form update

--- frontend/src/components/forms/loginForm/index.tsx
+++ frontend/src/components/forms/loginForm/index.tsx
@@ -1,55 +1,3 @@
-// import { SyntheticEvent, useState } from 'react';
-// import { Formik, Form, Field, ErrorMessage } from "formik";
-// function LoginForm() {
-
-//   return (
-//     <Formik
-//       initialValues={{ email: "", password: "" }}
-//       validate={(values) => {
-//         const errors = {};
-//         if (!values.email) errors.email = "Required";
-//         if (!values.password) errors.password = "Required";
-//         return errors;
-//       }}
-// onSubmit={async (values, { setSubmitting, resetForm }) => {
-//   try {
-//     const response = await axios({
-//       method: "POST",
-//       url: "http://localhost:8888/api/auth/login",
-//       data: values,
-//     });
-//     setSubmitting(false);
-
-//     console.log(response?.headers);
-//     resetForm();
-//   } catch (error) {
-//     setSubmitting(false);
-//     console.log(error);
-//   }
-// }}
-//       onSubmit={async (values) => {
-//         console.log(values)
-//       }}
-//     >
-//       {({ isSubmitting }) => (
-//         <Form>
-//           <label className="label">Email</label>
-//           <Field type="email" name="email" />
-//           <ErrorMessage name="email" component="div" />
-//           <label className="label">Password</label>
-//           <Field type="password" name="password" />
-//           <ErrorMessage name="password" component="div" />
-//           <button type="submit" disabled={isSubmitting}>
-//             Login
-//           </button>
-//         </Form>
-//       )}
-//     </Formik>
-//   );
-// }
-
-// export default LoginForm;
-
 import {
   Box,
   Button,
@@ -122,8 +70,6 @@ export function LoginForm() {
                 console.log(values);
                 const response = await login(values).unwrap();
                 console.log(response);
-                // const response = await addNewUser(values).unwrap();
-                // console.log(response);
                 // actions.resetForm();
                 // onClose();
               }}
