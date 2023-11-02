f6edcd915fde72c1cb5aee9d9bb19387c475f2eb
unknown <madan.serghei@yahoo.com>
2023-11-02 10:07:36 -0700

    FRONTEND : resolved loginForm conflict merge

--- frontend/src/components/forms/loginForm/index.tsx
+++ frontend/src/components/forms/loginForm/index.tsx
@@ -1,10 +1,10 @@
 import {
-  // Checkbox,
-  // Divider,
-  // HStack,
   Box,
   Button,
   Container,
+  // Checkbox,
+  // Divider,
+  // HStack,
   FormControl,
   FormLabel,
   Heading,
@@ -14,14 +14,11 @@ import {
   Text,
   FormErrorMessage,
 } from "@chakra-ui/react";
-import { useDispatch } from "react-redux";
-import { setCredentials } from "../../../features/auth/slice/authSlice";
-
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Formik, Form, Field } from "formik";
+import { useCookies } from "react-cookie";
 import { useLoginMutation } from "../../../app/api/apiSlice";
 import logo from "/logo.png";
-// import { useCookies } from "react-cookie";
 // import { OAuthButtonGroup } from "./OAuthButtonGroup";
 // import { PasswordField } from "./PasswordField";
 
@@ -37,9 +34,8 @@ function validateInput() {
 
 export function LoginForm() {
   const [login] = useLoginMutation();
-  const dispatch = useDispatch()
-  // const navigate = useNavigate();
   // const [cookies, setCookie] = useCookies(["refreshToken"]);
+  const navigate = useNavigate();
   // console.log(cookies)
   return (
     <Container
@@ -49,7 +45,7 @@ export function LoginForm() {
     >
       <Stack spacing="8">
         <Stack spacing="6">
-          <Image src={logo} sizes="sm" className="" alt="ESI Logo" />
+          {/* <Image src={logo} sizes="sm" className="" alt="ESI Logo" /> */}
           <Stack spacing={{ base: "2", md: "3" }} textAlign="center">
             <Heading size={{ base: "xs", md: "sm" }}>
               Log in to your account
@@ -73,26 +69,19 @@ export function LoginForm() {
                 password: "",
               }}
               onSubmit={async (values, actions) => {
-                try {
-                  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                  // @ts-ignore
-                  // const response = await login(values).unwrap();
-                  const { accessToken }  = await login(values).unwrap();
-                  console.log(accessToken)
-                  if (!accessToken) {
-                    console.log('Error, access token not available')
-                  } else {
-                    // console.log(accessToken);
-                    dispatch(setCredentials({accessToken}))
-                    actions.resetForm();
-                    // navigate("/products");
-                  }
-                } catch(error){
-                  console.log(error)
+                // console.log(values);
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                const response = await login(values).unwrap();
+                if (response) {
+                  console.log(response);
+                  actions.resetForm();
+                  navigate("/products");
                 }
+                // onClose();
               }}
             >
-              {(props: FormikProps<FormValues>) => (
+              {(props) => (
                 <Form>
                   <Field name="email" validate={validateInput}>
                     {({ field, form }: never) => (
