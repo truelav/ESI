1da72755b0492db310afe4234aac90b1e8ec7d9a
unknown <madan.serghei@yahoo.com>
2023-11-05 14:44:13 -0800

    FRONTEND : edit | styled the homepage

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,21 +1,26 @@
 import { Link } from "react-router-dom";
-// import { useDisclosure, Button } from "@chakra-ui/react";
+import { Container, Flex } from "@chakra-ui/react";
 // import ImportProductsModal from "../../shared/ui/Modals/ImportProducts/ImportProductsModal";
 
 export default function Home() {
   // const { isOpen, onOpen, onClose } = useDisclosure()
   return (
-    <div className="home-container" id="root">
-      <h2>You got products to sell or to buy ?</h2>
-      <p>
-        Please Login using provided credentials or{" "}
-        <Link to="contact">contact us</Link> in order to sign up for an account
-      </p>
-      {/* <Button className="" onClick={onOpen}>
-            <p>Import Products</p>
-      </Button> */}
-      {/* <ImportProductsModal isOpen={isOpen} onClose={onClose}/> */}
-      <Link to="products">Find your products</Link>
-    </div>
+    <Container bg="#ededee" maxW="full" mt={0} centerContent overflow="hidden">
+      <Flex>
+        <div className="home-container" id="root">
+          <h2>You got products to sell or to buy ?</h2>
+          <p>
+            Please Login using provided credentials or{" "}
+            <Link to="contact">contact us</Link> in order to sign up for an
+            account
+          </p>
+          {/* <Button className="" onClick={onOpen}>
+              <p>Import Products</p>
+        </Button> */}
+          {/* <ImportProductsModal isOpen={isOpen} onClose={onClose}/> */}
+          <Link to="products">Find your products</Link>
+        </div>
+      </Flex>
+    </Container>
   );
 }
