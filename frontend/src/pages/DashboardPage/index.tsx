252c38b0f4f2f6411b238c7f85c2cbc07437ec04
unknown <madan.serghei@yahoo.com>
2023-11-05 17:39:04 -0800

    FRONTEND: dashboard upload image and save

--- frontend/src/pages/DashboardPage/index.tsx
+++ frontend/src/pages/DashboardPage/index.tsx
@@ -1,5 +1,5 @@
 import { Container, Text } from "@chakra-ui/react";
-import { StatGroup  } from "../../components/stats/StatGroup/StatGroup";
+import { StatGroup } from "../../components/stats/StatGroup/StatGroup";
 
 export default function Dashboard() {
   return (
@@ -7,10 +7,20 @@ export default function Dashboard() {
       <Container>
         <Text>Welcome to Dashboard</Text>
       </Container>
-          <StatGroup />
-          <StatGroup/>
+      <StatGroup />
+      <StatGroup />
       <Container>
         <Text>More Info will come soon</Text>
+      </Container>
+      <Container>
+        <form
+          action="http://localhost:8888/api/products/upload"
+          method="POST"
+          encType="multipart/form-data"
+        >
+          <input type="file" name="image" />
+          <button type="submit">Upload</button>
+        </form>
       </Container>
     </>
   );
