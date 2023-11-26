9971cd94adbf650f92f684fd9dad32d723874bd5
serghei madan <madan.serghei@yahoo.com>
2023-11-26 11:04:43 -0800

    FRONTEND | editing dash home page, need to create User Profile Page

---
+++ frontend/src/app/config/StateSchema.ts
@@ -0,0 +1,6 @@
+import { ProductSchema } from "../../entities/Product/model/types/product";
+
+export interface StateSchema {
+    product: ProductSchema
+}
+
--- frontend/src/pages/DashboardPage/index.tsx
+++ frontend/src/pages/DashboardPage/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Text } from "@chakra-ui/react";
+import { Container, Grid, GridItem, Heading, Text, Highlight } from "@chakra-ui/react";
 import { useDispatch } from "react-redux";
 
 import { useGetAllProductsQuery } from "../../app/api/apiSlice";
@@ -6,8 +6,65 @@ import { setProducts, setCategoryProducts, setTotalProducts } from "../../entiti
 
 import { StatGroup } from "../../components/stats/StatGroup/StatGroup";
 import { Product } from "../../entities/Product/model/types/product";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
+    return (
+        <>
+
+            <Container maxW="full"  minH="700px" mt="100px" centerContent overflow="hidden">
+                <Grid templateColumns='repeat(12, 1fr)' gap={6}>
+                    <GridItem colSpan={10}>
+                        <Heading as='h1' size='4xl' noOfLines={3}>
+                            <span color="blue">Welcome</span> to Your Dashboard Profile Page
+                        </Heading>
+                    </GridItem>
+                    <GridItem colSpan={6}>
+                        <Heading as='h2' size='xl'>
+                            You Can manage : 
+                            <br/>
+                            <Link to="products">
+                                <Highlight
+                                    query={['Products']}
+                                    styles={{ px: '1', py: '1', rounded: 'full', bg: 'blue.100' }}
+                                >
+                                        Products
+                                </Highlight>
+                            </Link>
+                            <br/>
+                            <Link to="users">
+                                <Highlight
+                                    query={['Users']}
+                                    styles={{ px: '1', py: '1', rounded: 'full', bg: 'blue.100' }}
+                                >
+                                        Users
+                                </Highlight>
+                            </Link>
+                            <br/>
+                             or create 
+                             <br/>
+                             <Link to="presentation">
+                                <Highlight
+                                    query={['Presentation']}
+                                    styles={{ px: '1', py: '1', rounded: 'full', bg: 'blue.100' }}
+                                >
+                                        Presentation
+                                </Highlight>
+                            </Link>
+                        </Heading>
+                    </GridItem>
+                    <GridItem colSpan={4}>
+                        <Text my={6}>
+                            Welcome to our innovative B2B platform designed to streamline your business procurement process. Getting started is as easy as creating an account, granting you immediate access to a world of exclusive benefits. Once registered, you'll unlock a personalized catalog tailored to your specific business needs. Navigate through a comprehensive range of products and services, each curated to meet the demands of your industry. 
+                        </Text>
+                    </GridItem>
+                </Grid>
+            </Container>    
+        </>
+    )
+}
+
+export const AnalyicsPage = () => {
     const {
         data: products,
         isLoading,
@@ -35,7 +92,6 @@ export default function Dashboard() {
             totalCategories: 11
         }
 
-        // Convert the grouped products into an array of objects
         const categoryProducts = Object.entries(groupedProducts).map(
             ([brand, products]) => ({
                 brand,
@@ -71,9 +127,6 @@ export default function Dashboard() {
 
         content = (
             <>
-                <Container>
-                    <Text>Welcome to Dashboard</Text>
-                </Container>
                 <StatGroup />
             </>
         )
