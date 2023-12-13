df66d796603d59ec752c343da6f18381d2bfc39a
unknown <madan.serghei@yahoo.com>
2023-12-12 18:29:34 -0800

    FRONTEND | DashUsersPage remove dash stats

--- frontend/src/pages/DashboardPage/DashUsersPage/DashUsersPage.tsx
+++ frontend/src/pages/DashboardPage/DashUsersPage/DashUsersPage.tsx
@@ -1,35 +1,42 @@
-import { Button, Text, ButtonGroup, Grid, GridItem, Heading } from "@chakra-ui/react";
+import {
+    Button,
+    Text,
+    ButtonGroup,
+    Grid,
+    GridItem,
+    Heading,
+} from "@chakra-ui/react";
 
 import { UsersList } from "../../../features/users/ui/usersList/usersList";
 
 import CreateUserModal from "../../../shared/ui/Modals/CreateUser/CreateUserModal";
-import { StatUserGroup } from "../../../components/stats/StatGroup/StatUsersGroup";
 
 function DashUsersPage() {
-
-  return (
-    <>
-        <Grid templateColumns='repeat(12, 1fr)' gap={6}>
-              <GridItem colSpan={12}>
-                    <Heading as='h1' size='xl' noOfLines={1}>Welcome to Users Page</Heading>
-              </GridItem>
-              <GridItem colSpan={8}>
-                  < StatUserGroup />
-              </GridItem>
-              <GridItem colSpan={4}>
-                  <ButtonGroup>
-                    <CreateUserModal/>
-                    <Button className="dash_products_nav_button"  colorScheme='red'>
-                      <Text>Delete Users</Text>
-                    </Button>
-                  </ButtonGroup>
-              </GridItem>
-              <GridItem colSpan={12}>
-                  <UsersList />
-              </GridItem>
-          </Grid>
-    </>
-  );
+    return (
+        <>
+            <Grid templateColumns="repeat(12, 1fr)" gap={6}>
+                <GridItem colSpan={12}>
+                    <Heading as="h1" size="xl" noOfLines={1}>
+                        Welcome to Users Page
+                    </Heading>
+                </GridItem>
+                <GridItem colSpan={4}>
+                    <ButtonGroup>
+                        <CreateUserModal />
+                        <Button
+                            className="dash_products_nav_button"
+                            colorScheme="red"
+                        >
+                            <Text>Delete Users</Text>
+                        </Button>
+                    </ButtonGroup>
+                </GridItem>
+                <GridItem colSpan={12}>
+                    <UsersList />
+                </GridItem>
+            </Grid>
+        </>
+    );
 }
 
 export default DashUsersPage;
