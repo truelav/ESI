aa5f6a226aa53fe1bcaf66a2023c6119a85921b6
serghei madan <madan.serghei@yahoo.com>
2023-12-18 14:51:51 -0800

    FRONTEND | DashOrders Page created and getAllOrders slice

--- frontend/src/pages/DashboardPage/DashOrders/DashOrdersPage.tsx
+++ frontend/src/pages/DashboardPage/DashOrders/DashOrdersPage.tsx
@@ -1,18 +1,44 @@
 import { Container, Text } from "@chakra-ui/react";
 
-
+import { useGetAllOrdersQuery } from "../../../app/api/apiSlice";
 
 function DashOrders() {
-    return (
-        <>
+    const {
+        data: ordersData,
+        isLoading,
+        isSuccess,
+        isError,
+        error,
+    } = useGetAllOrdersQuery();
+
+    let content = (<div></div>)
+
+    if(isLoading){
+        return (<p>Loading..</p>)
+    } 
+
+    if (isError) {
+        return  <p className="errmsg">No Users found {JSON.stringify(error)}</p>
+    }
+
+    if (isSuccess) {
+        content = (
+            <div>
+                {/* {usersData?.map((user: User) =>  (
+                        <UsersListItem user={user} key={user._id} />
+                ))} */}
             <Container>
                 <Text> Welcome to Orders</Text>
             </Container>
             <Container>
                 <Text> Coming Soon</Text>
             </Container>
-        </>
-    )
+            </div>
+        )
+    }
+    console.log(ordersData)
+  
+    return content
 }
 
 export default DashOrders
