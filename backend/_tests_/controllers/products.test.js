0b8057af637d8495207a3d88dfc7e4ef4e2dec6d
serghei madan <madan.serghei@yahoo.com>
2023-12-22 12:12:51 -0800

    BACKEND | adding tests for Products Controler

--- backend/_tests_/controllers/products.test.js
+++ backend/_tests_/controllers/products.test.js
@@ -2,20 +2,45 @@ import request from "supertest"
 import mongoose from "mongoose"
 import createError from 'http-errors';
 import { HTTPStatusCodes } from "../../utils/constants.js";
+import products from "../testdata/product/products"
 import Product from "../../models/Product/Product.js"
 import app from "../../index.js"
 
+const baseURL = "http://localhost:8888/api"
+
+const addProductData = {
+    brand: "TestBrand",
+    model: "TestModel",
+    description: "TestDescription",
+    category: "TestCategory",
+    subcategory: "TestSubcategory",
+    upc: "TestUPC",
+    price: 15,
+    quantity: 10,
+
+}
+
 describe('Product Routes', () => {
-    beforeAll(async () => {
-         await mongoose.connect("mongodb://127.0.0.1:27017/ESI")
+    // beforeAll(async () => {
+    //      await mongoose.connect("mongodb://127.0.0.1:27017/ESI")
+    // })
+
+    // afterAll(async () => {
+    //     await mongoose.disconnect()
+    // })
+
+    it('GET /api/products It should return all products', async () => {
+        const res = await request("http://localhost:8888/api").get('/products')
+
+        console.log(res.body)
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveLength(5);
+
+        return res    
     })
 
-    afterAll(async () => {
-        await mongoose.disconnect()
-    })
-
-    test('POST /api/products It Should add a new product', async () => {
-        const response = await request(app)
+    it('POST /api/products It Should add a new product', async () => {
+        const res = await request(app)
             .post('/api/products')
             .field('brand', 'TestBrand')
             .field('model', 'TestModel')
@@ -28,19 +53,10 @@ describe('Product Routes', () => {
             // .attach('image', 'http://localhost:8888/static/images/PHILIPS-HD974199-1703123972471.jpg')
             // .send();
         
-        expect(response.status).toBe(200)
-        expect(response.body).toHaveProperty('message', expect.any(String)) 
-        expect(response.body).toHaveProperty('newProduct');
+        expect(res.status).toBe(200)
+        expect(res.body).toHaveProperty('message', expect.any(String)) 
+        expect(res.body).toHaveProperty('newProduct');
 
-        const { newProduct } = response.body;
-        // Check if the product was saved in the database
-        const savedProduct = await Product.findById(newProduct._id);
-        expect(savedProduct).not.toBeNull();
-        expect(savedProduct.images).toContain('fallback_image.jpeg'); // Assuming fallback_image.jpeg is used for testing
-
-        // Clean up: Delete the test product
-        await Product.deleteOne({ _id: newProduct._id });
-
-        return
+        return res
     })
 })
