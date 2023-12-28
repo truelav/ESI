2764feb786b4c4826a9690c3e1d8baa56571f00f
serghei madan <madan.serghei@yahoo.com>
2023-12-27 17:30:07 -0800

    BACKEND | getSingleUser route added

--- backend/routes/Auth/auth.js
+++ backend/routes/Auth/auth.js
@@ -10,6 +10,7 @@ router.post("/login", AuthControllers.login);
 router.post("/logout", AuthControllers.logout);
 router.get("/refresh", AuthControllers.refresh);
 router.get("/users", AuthControllers.getUsers);
+router.get("/users/:id", AuthControllers.getSingleUser);
 router.post("/users/", AuthControllers.register);
 router.post("/users/edit", editUser);
 router.delete("/users/:id", AuthControllers.deleteUser);
