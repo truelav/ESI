b6c2291cb5b510b77f3529fc7a9024ac4464826f
serghei madan <madan.serghei@yahoo.com>
2024-01-04 17:26:02 -0800

    BACKEND | setting up integration testing and testDB

--- backend/_tests_/controllers/products.test.js
+++ backend/_tests_/controllers/products.test.js
@@ -1,20 +1,20 @@
-import request from "supertest"
-import mongoose from "mongoose"
-import products from "../testdata/product/products"
-import Product from "../../models/Product/Product.js"
+// import request from "supertest"
+// import mongoose from "mongoose"
+// import products from "../testdata/product/products"
+// import Product from "../../models/Product/Product.js"
 
-const addProductData = {
-    brand: "TestBrand",
-    model: "TestModel",
-    description: "TestDescription",
-    category: "TestCategory",
-    subcategory: "TestSubcategory",
-    upc: "TestUPC",
-    price: 15,
-    quantity: 10,
+// const addProductData = {
+//     brand: "TestBrand",
+//     model: "TestModel",
+//     description: "TestDescription",
+//     category: "TestCategory",
+//     subcategory: "TestSubcategory",
+//     upc: "TestUPC",
+//     price: 15,
+//     quantity: 10,
 
-}
+// }
 
-it('should return all products', () => {
+// it('should return all products', () => {
 
-})
+// })
