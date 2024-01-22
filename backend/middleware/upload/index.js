64402bd6b71bfae2d941767c722e9019a9ccbd79
serghei madan <madan.serghei@yahoo.com>
2024-01-22 15:42:07 -0800

    BACKEND | need to figure what to use for Image names to avoid duplicates

--- backend/middleware/upload/index.js
+++ backend/middleware/upload/index.js
@@ -1,6 +1,8 @@
 import multer from "multer";
 import path from "path";
 
+import { createFilename } from "../../utils/files/files.js";
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     console.log(req.body)
@@ -10,22 +12,21 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     // cb(null, file.originalname);
     
-    const uniqueSuffix = ("" +  Date.now()).trim();
-    const brandName = req.body.brand.split(" ").join("")
-    const modelName = req.body.model.split(" ").join("")
+    const data = req.body
+    const fileName = createFilename(data)
     const extName = path.extname(file.originalname)
-    const fileName = brandName + '-' + modelName + '-' + uniqueSuffix
+
     cb(null, fileName + extName);
   },
   limits: {
     fileSize: 500000,
   },
-  //   fileFilter(req, file, cb) {
-  //     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-  //       return cb(new Error("Please upload a valid image file"));
-  //     }
-  //     cb(undefined, true);
-  //   },
+    fileFilter(req, file, cb) {
+      if (!file.originalname.match(/\.(jpg|jpeg|png|webp)$/)) {
+        return cb(new Error("Please upload a valid image file"));
+      }
+      cb(undefined, true);
+    },
 });
 
 export const upload = multer({ storage });
